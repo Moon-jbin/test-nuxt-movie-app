ed4b605c69f7585c9582f00b8b660a64f95daa00
Moon-jbin <mjb1209@naver.com>
2021-06-28 16:06:33 +0900

    Add nuxt project

---
+++ store/about.js
@@ -0,0 +1,10 @@
+export default {
+  namespaced: true,
+  state: () => ({
+    name: 'Moon-jbin',
+    email: 'mjb1209@naver.com',
+    blog: 'https://blog.naver.com/mjb1209',
+    phone: '+82-10-5093-9512',
+    image: 'https://cdn.pixabay.com/photo/2017/12/25/10/47/toy-3038204_960_720.jpg'
+  })
+}
